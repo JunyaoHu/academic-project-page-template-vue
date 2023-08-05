5635dade171b540a7a5b149194388b7744c53f88
hujunyao <1078622540@qq.com>
2023-08-06 03:39:18 +0800

    [Fix] Carousel display

--- src/components/sections/Carousel.vue
+++ src/components/sections/Carousel.vue
@@ -3,11 +3,11 @@ export default {
     data() {
     return {
         source_paths: [
-        "/carousel/1.jpg",
-        "/carousel/2.jpg",
-        "/carousel/3.jpg",
-        "/carousel/4.jpg",
-        "/carousel/5.jpg",
+        "./carousel/1.jpg",
+        "./carousel/2.jpg",
+        "./carousel/3.jpg",
+        "./carousel/4.jpg",
+        "./carousel/5.jpg",
       ],
     }
   }
