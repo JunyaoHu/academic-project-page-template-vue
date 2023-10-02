47cc615ac914b0cd70d34a44171e530aa461f7c5
hujunyao <1078622540@qq.com>
2023-10-02 19:56:24 +0800

    [Add] support showing web video (bilibili and youtube)

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -4,6 +4,7 @@ import Title from './sections/Title.vue'
 import Abstract from './sections/Abstract.vue'
 import BibTeX from './sections/BibTeX.vue'
 import Carousel from './sections/Carousel.vue'
+import Video from './sections/Video.vue'
 
 export default {
   components: {
@@ -11,12 +12,14 @@ export default {
     Abstract,
     BibTeX,
     Carousel,
+    Video,
   }
 }
 </script>
 
 <template>
     <Title/>
+    <Video/>
     <Carousel/>
     <Abstract/>
     <BibTeX/>
---
+++ src/components/sections/Video.vue
@@ -0,0 +1,39 @@
+<script>
+export default {
+  data() {
+    return {
+      a: 'bb'
+    }
+  },
+}
+
+</script>
+
+<template>
+  <div class="bg">
+    
+    <el-row>
+      <h1 class="title1">Explanatory Video</h1>
+    </el-row>
+
+    <el-row justify="space-evenly">
+        <el-col :xs="20" :sm="20" :md="10" :lg="10" :xl="10">
+            <iframe src="//player.bilibili.com/player.html?aid=802517980&bvid=BV1qy4y1x7VV&cid=325180260&p=1" scrolling="no" border="0" frameborder="no" framespacing="0" allowfullscreen="true"> </iframe>
+        </el-col>
+        <el-col :xs="20" :sm="20" :md="10" :lg="10" :xl="10">
+            <iframe src="https://www.youtube.com/embed/1kE_VJgM4u8?si=JZ_XoMOZNfVG1lAF" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        </el-col>
+    </el-row>
+
+  </div>
+</template>
+
+<style>
+@media screen{
+iframe {
+    position: center;
+    width: 100%;
+    height: 425px;
+    padding: 20px 0; 
+}}
+</style>
