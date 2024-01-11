39b56e62ebbf5ff6bd9b1cb7a8ccf32d9881e652
hujunyao <1078622540@qq.com>
2024-01-11 19:38:15 +0800

    [Update]

--- src/components/sections/Comment.vue
+++ src/components/sections/Comment.vue
@@ -1,4 +1,4 @@
-<script>
+<!-- <script>
 twikoo.init({
   envId: 'https://blog-comment-h866w5ycd-junyaohu.vercel.app/', // 腾讯云环境填 envId；Vercel 环境填地址（https://xxx.vercel.app）
   el: '#tcomment', // 容器元素
@@ -6,9 +6,9 @@ twikoo.init({
   path: window.location.pathname, // 用于区分不同文章的自定义 js 路径，如果您的文章路径不是 location.pathname，需传此参数
   lang: 'zh-CN', // 用于手动设定评论区语言，支持的语言列表 https://github.com/twikoojs/twikoo/blob/main/src/client/utils/i18n/index.js
 })
-</script>
+</script> -->
 
-<template>
+<!-- <template>
   <div>
     <el-row>
       <h1 class="title1">Comment</h1>
@@ -19,4 +19,4 @@ twikoo.init({
       </el-col>
     </el-row>
   </div>
-</template>
+</template> -->
