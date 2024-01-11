042192a7a303f78f79da2bee2aad9e3662264e01
hujunyao <1078622540@qq.com>
2024-01-11 19:12:06 +0800

    Revert "[Add] twikoo comment"
    This reverts commit 75b1421858bf34b90a26055ad84247fb4789ed33.

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -6,7 +6,6 @@ import Carousel from './sections/Carousel.vue'
 import Video from './sections/Video.vue'
 import ImageSlider from './sections/ImageSlider.vue'
 import Echart from './sections/Echart.vue'
-import Comment from './sections/Comment.vue'
 
 export default {
   components: {
@@ -17,7 +16,6 @@ export default {
     Video,
     ImageSlider,
     Echart,
-    Comment,
   }
 }
 </script>
@@ -30,5 +28,4 @@ export default {
     <Echart/>
     <ImageSlider/>
     <BibTeX/>
-    <Comment/>
 </template>
